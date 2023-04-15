939a8fe498073c74bde7985d8c7680af85993db3
An Van Nguyen <83855206+anvndev@users.noreply.github.com>
2023-04-15 21:58:16 +0700

    input (num1&num2)
    fix

--- cal.py
+++ cal.py
@@ -10,6 +10,11 @@ Please type in the math operation you would like to complete:
 / for division
 ''')
 
+#     int
+    number_1 = int(input('Please enter the first number: '))
+    number_2 = int(input('Please enter the second number: '))
+    
+    
      if operation == '+':
         print('{} + {} = '.format(number_1, number_2))
         print(number_1 + number_2)
