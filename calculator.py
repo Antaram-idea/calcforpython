15c9ee7bd696d9409305d5903b1a6adb068fe5a1
An Van Nguyen <83855206+anvndev@users.noreply.github.com>
2023-04-15 21:46:24 +0700

    first

--- calculator.py
+++ calculator.py
@@ -8,3 +8,6 @@ Please type in the math operation you would like to complete:
 * for multiplication
 / for division
 ''')
+# first
+    number_1 = int(input('Please enter the first number: '))
+    number_2 = int(input('Please enter the second number: '))
