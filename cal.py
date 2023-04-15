122d131d820a102291245b8ed6ee5cbfc855d74b
An Van Nguyen <83855206+anvndev@users.noreply.github.com>
2023-04-15 21:55:34 +0700

    elseif
    else and if for number

--- cal.py
+++ cal.py
@@ -9,3 +9,22 @@ Please type in the math operation you would like to complete:
 * for multiplication
 / for division
 ''')
+
+     if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+
+    else:
+        print('You have not typed a valid operator, please run the program again.')
